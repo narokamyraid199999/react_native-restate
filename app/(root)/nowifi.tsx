802cc390552cdbfe0ba2d8b6947225130ff1ae5d
narokamyraid19999 <narokamyraid19999@gmail.com>
2026-05-24 14:05:51 -0700

    feat: update nowifi screen design

--- app/(root)/nowifi.tsx
+++ app/(root)/nowifi.tsx
@@ -1,14 +1,17 @@
-import { Text, View } from "react-native";
+import icons from "@/constants/icons";
+import { Image, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function NoWifi() {
   return (
-    <View className="bg-white h-full flex flex-col justify-center items-center gap-4">
+    <SafeAreaView className="bg-white h-full flex flex-col justify-center items-center gap-4">
+      <Image source={icons.wifi} className="size-24" />
       <Text className="text-2xl font-bold text-gray-800">
         No Internet Connection
       </Text>
       <Text className="text-gray-600">
         Please check your network settings and try again.
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
